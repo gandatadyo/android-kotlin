d35d4d260f40d64d76831abee7b250e69b5afbb9
gandatadyo <gandatadyosurya@yahoo.com>
2020-02-19 19:53:58 +0700

    test

--- app/src/main/java/com/module/kotlin/ListviewActivity.kt
+++ app/src/main/java/com/module/kotlin/ListviewActivity.kt
@@ -13,13 +13,28 @@ class ListviewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listview)
 
+        // inisialisasi data awal ketika muncul
         val dataarray = listOf("Informatika","Manajemen","Hukum","Desain Grafis","Akutansi")
         val adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, dataarray)
         listview.adapter = adapter
 
+
+        // inisialisasi listview onlclick
         val mMessageClickedHandler = OnItemClickListener { parent, v, position, id ->
             toasttest(dataarray[position], this)
         }
         listview.onItemClickListener = mMessageClickedHandler
+
+        // inisialisasi reload data
+        btnReset.setOnClickListener {
+            ResetData()
+        }
+    }
+
+    private fun ResetData(){
+        // reload data (reset)
+        val dataarray = listOf("Tata Boga","Sastra Inggris","Teknik Industri","Elektro")
+        val adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, dataarray)
+        listview.adapter = adapter
     }
 }
