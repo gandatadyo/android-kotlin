8aeee44d0e0996e9a394f905003a243a11282761
gandatadyo <gandatadyosurya@yahoo.com>
2021-07-07 17:16:12 +0700

    update image cropper

--- app/src/main/java/com/module/kotlin/RecylerViewActivity.kt
+++ app/src/main/java/com/module/kotlin/RecylerViewActivity.kt
@@ -9,7 +9,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import com.module.kotlin.R
 import kotlinx.android.synthetic.main.activity_recyler_view.*
 
 class RecylerViewActivity : AppCompatActivity() {
